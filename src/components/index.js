9980d63ba8f44f2ec49f6715852d1f83c872b7c9
Phuong Thanh <phuong74200@gmail.com>
2022-06-01 09:06:54 +0700

    login

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 import Button from './Button';
+import Particles from './Particles';
 
-export { Button };
+export { Button, Particles };
---
+++ src/routes/Login/index.js
@@ -0,0 +1,79 @@
+import axios from 'axios';
+import { GoogleLogin } from 'react-google-login';
+
+import smImg1 from '../../assets/LoginHero/image 2.png';
+import smImg2 from '../../assets/LoginHero/image 3.png';
+import { Particles, BigImg, SmallImg } from '../../components';
+import {
+    GoogleButton,
+    StyledContainer,
+    StyledTitle,
+    StyledWelcome,
+    StyledParagraph,
+    ParticlesContainer,
+    ImageGroup,
+    StyledForm,
+} from './style';
+
+const Login = () => {
+    document.title = 'Login';
+
+    const URL = process.env.REACT_APP_API_URL + '/auth/accounts/';
+
+    const responseGoogle = (response) => {
+        console.log(response);
+        axios.post(URL, {
+            idToken: response.tokenId,
+            provider: 'GOOGLE',
+        });
+        console.log({
+            url: URL,
+            data: {
+                idToken: response.tokenId,
+                provider: 'GOOGLE',
+            },
+        });
+    };
+
+    const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
+    return (
+        <>
+            <StyledContainer>
+                <StyledForm>
+                    <StyledTitle>
+                        <span>F</span>PLMS
+                    </StyledTitle>
+                    <StyledWelcome>Welcome to FPLMS</StyledWelcome>
+                    <StyledParagraph>You are just one step away from your projects</StyledParagraph>
+                    <GoogleLogin
+                        clientId={CLIENT_ID}
+                        buttonText="Login"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={'single_host_origin'}
+                        disabledStyle
+                        render={(btn) => (
+                            <GoogleButton onClick={btn.onClick}>
+                                Sign in with FPT email
+                            </GoogleButton>
+                        )}
+                    />
+                    <StyledParagraph isBold>
+                        By logging in, you accept with our <span>Terms & Conditions</span>
+                    </StyledParagraph>
+                </StyledForm>
+                <ImageGroup>
+                    <BigImg />
+                    <SmallImg timing={10} bottom={150} left={1} src={smImg2}></SmallImg>
+                    <SmallImg timing={15} bottom={100} right={1} src={smImg1}></SmallImg>
+                </ImageGroup>
+            </StyledContainer>
+            <ParticlesContainer>
+                <Particles />
+            </ParticlesContainer>
+        </>
+    );
+};
+
+export default Login;
