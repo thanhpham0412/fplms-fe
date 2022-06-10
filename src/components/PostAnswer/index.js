c01a4d9f362ae9e58feceffe9309a075fa41dcbb
KenNguyen <ngkien299@gmail.com>
2022-06-10 12:39:52 +0700

    discussion view

--- src/components/PostAnswer/index.js
+++ src/components/PostAnswer/index.js
@@ -5,6 +5,11 @@ const PostAnswer = () => {
         <Container>
             <Avatar />
             <CommentInput placeholder="Writing your comment" />
+            <Avatar />
+            <CommentInput
+                disabled
+                defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+            />
         </Container>
     );
 };
