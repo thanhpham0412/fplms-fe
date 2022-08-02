cc46e547bda0c0259735ab04421557c75aaaf2d2
Phuong Thanh <phuong74200@gmail.com>
2022-08-02 14:16:44 +0700

    change ws

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -62,7 +62,7 @@ const Header = () => {
     const user = getTokenInfo();
 
     useEffect(() => {
-        const socket = io('ws://8.tcp.ngrok.io:14651', {
+        const socket = io('ws://0.tcp.ngrok.io:12722', {
             extraHeaders: {
                 Authorization: localStorage.getItem('token'),
             },
